65243ff8918917a88179ca8c00ed9363bd4bb0f0
Satish1112 <pottipatisatti@gmail.com>
2023-12-25 14:44:31 +0530

    check

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/newsDetails" element={<NewsDetails />} />
